d9203673f2788c6027155a66ed2258202873a2c1
Denis Skovpen <deniskovpen@gmail.com>
2020-05-29 17:23:11 +0300

    Add telegram action calling while photos being sent

--- main.py
+++ main.py
@@ -9,6 +9,34 @@ import traceback
 import urllib3
 
 
+class TGAction:
+
+    def __init__(self, bot, chat_id, action, period=4):
+        self.bot = bot
+        self.action = action
+        self.chat_id = chat_id
+        self.period = period
+
+    async def update(self):
+        while True:
+            await self.bot.send_chat_action(self.chat_id, self.action)
+            await asyncio.sleep(self.period)
+
+    async def __aenter__(self):
+        self.task = asyncio.get_event_loop().create_task(self.update())
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if not self.task:
+            return
+        if not self.task.cancelled():
+            self.task.cancel()
+            try:
+                await self.task
+            except asyncio.CancelledError:
+                pass
+
+
 class NoUrlError(Exception):
     pass
 
@@ -157,12 +185,13 @@ async def handle_request(bot, chat_id, msg_txt):
     if len(urls) == 0:
         raise NoUrlError()
     direct_urls = await get_img_links(urls[0])
-    for u in direct_urls:
-        try:
-            await bot.send_photo(chat_id, u)
-            await asyncio.sleep(0.1)
-        except TelegramAPIError as e:
-            print(e.__class__.__name__ + ': ' + str(e))
+    async with TGAction(bot, chat_id, "upload_photo"):
+        for u in direct_urls:
+            try:
+                await bot.send_photo(chat_id, u)
+                await asyncio.sleep(0.1)
+            except TelegramAPIError as e:
+                print(e.__class__.__name__ + ': ' + str(e))
     return None
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
